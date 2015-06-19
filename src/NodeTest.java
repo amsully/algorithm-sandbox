5ab902d60449db4ba922462db0749465f6b25f86
amsully <sullivan1513@gmail.com>
2015-06-18 22:14:26 -0400

    Final test case to confirm algo

--- src/NodeTest.java
+++ src/NodeTest.java
@@ -68,6 +68,41 @@ public class NodeTest {
 			assertEquals( rootInverted.getLeftNode().getRightNode().value, new Integer(6));
 			assertEquals( rootInverted.getRightNode().getRightNode().value, new Integer(4));
 	}
-	
+
+	@Test
+	public void testInversionOfSixNodeTree()
+	{
+		/**
+		 * Original
+		 * 			    1
+		 * 			  /   \
+		 * 		     2 	   3
+		 * 			  \   / \
+		 * 		      5  6   7
+		 * Inverted
+		 *		        1
+		 * 			  /   \
+		 * 		     3 	   2
+		 * 			/ \   / 
+		 * 		   7   6 5   
+		 */
+			Node root = new Node(1);
+			Node leftNode = new Node(2);
+			Node rightNode = new Node(3);
+			root.setLeftNode(leftNode);			root.setRightNode(rightNode);
+			leftNode.setRightNode(new Node(5));
+			rightNode.setLeftNode(new Node(6));
+			rightNode.setRightNode(new Node(7));
+
+			
+			
+			TreeController tree = new TreeController(root);
+			
+			Node rootInverted = tree.invert(root);
+			assertEquals( rootInverted.getLeftNode().value, new Integer(3));
+			assertEquals( rootInverted.getRightNode().value, new Integer(2));
+			assertEquals( rootInverted.getLeftNode().getRightNode().value, new Integer(6));
+			assertEquals( rootInverted.getRightNode().getRightNode(), null);
+	}
 
 }
